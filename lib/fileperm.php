2f32fbc98a885a7ac051f9ccc5ad9b0b78e5cf64
Alexis Ducastel <alexis@ducastel.net>
2012-09-17 23:33:42 +0200

    fileperm for ll function

---
+++ lib/fileperm.php
@@ -0,0 +1,53 @@
+<?php
+function getFilePerm($file){
+    $perms = fileperms($file);
+    
+    if (($perms & 0xC000) == 0xC000) {
+        // Socket
+        $info = 's';
+    } elseif (($perms & 0xA000) == 0xA000) {
+        // Lien symbolique
+        $info = 'l';
+    } elseif (($perms & 0x8000) == 0x8000) {
+        // Régulier
+        $info = '-';
+    } elseif (($perms & 0x6000) == 0x6000) {
+        // Block special
+        $info = 'b';
+    } elseif (($perms & 0x4000) == 0x4000) {
+        // Dossier
+        $info = 'd';
+    } elseif (($perms & 0x2000) == 0x2000) {
+        // Caractère spécial
+        $info = 'c';
+    } elseif (($perms & 0x1000) == 0x1000) {
+        // pipe FIFO
+        $info = 'p';
+    } else {
+        // Inconnu
+        $info = 'u';
+    }
+    
+    // Autres
+    $info .= (($perms & 0x0100) ? 'r' : '-');
+    $info .= (($perms & 0x0080) ? 'w' : '-');
+    $info .= (($perms & 0x0040) ?
+                (($perms & 0x0800) ? 's' : 'x' ) :
+                (($perms & 0x0800) ? 'S' : '-'));
+    
+    // Groupe
+    $info .= (($perms & 0x0020) ? 'r' : '-');
+    $info .= (($perms & 0x0010) ? 'w' : '-');
+    $info .= (($perms & 0x0008) ?
+                (($perms & 0x0400) ? 's' : 'x' ) :
+                (($perms & 0x0400) ? 'S' : '-'));
+    
+    // Tout le monde
+    $info .= (($perms & 0x0004) ? 'r' : '-');
+    $info .= (($perms & 0x0002) ? 'w' : '-');
+    $info .= (($perms & 0x0001) ?
+                (($perms & 0x0200) ? 't' : 'x' ) :
+                (($perms & 0x0200) ? 'T' : '-'));
+    
+    return $info;
+}
